0ec4edf5e4fbdd0aec46da03ae6250c09a861b85
Ehsan <lancerf562@gmail.com>
2023-01-31 11:08:24 +0500

    'persistence_added'

--- v2.py
+++ v2.py
@@ -5,9 +5,8 @@ import undetected_chromedriver as uc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException,NoSuchElementException
 import os
 import coloredlogs, logging
 from argparse import ArgumentParser
@@ -15,7 +14,12 @@ import re
 import json
 import random
 import pyautogui
+import pickle
 
+
+
+class LoginRequired(Exception):
+    pass
 
 
 class Linkedin_Bot():
@@ -26,14 +30,14 @@ class Linkedin_Bot():
     def __init__(self):
         self.login_url = 'https://www.linkedin.com/login'
         self.logged_In = False
-        self.message = 'Hello'
         self.page_no = 1
-        self.connects_limit = 20
         self.count = 1
 
 
     def init_driver(self):
         self.driver = uc.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://www.linkedin.com/")
         self.action = ActionChains(self.driver)
 
 
@@ -51,14 +55,14 @@ class Linkedin_Bot():
         x, y = pyautogui.size()
         x = random.randint(0, x)
         y = random.randint(0, y)
-        pyautogui.moveTo(x, y, duration=0.5)
+        pyautogui.moveTo(x, y, duration=random.uniform(0.4, 0.7))
         time.sleep(random.choice(range(delay['min'], delay['max'])))
 
 
     def wait_for_element(self, path, timeout=10, retry=False):
         try:
             WebDriverWait(self.driver,timeout=timeout).until(EC.visibility_of_element_located((By.XPATH,path)))
-        except TimeoutException:
+        except (TimeoutException, NoSuchElementException):
             if retry:
                 return self.wait_for_element(path, timeout, retry=False)
             else:
@@ -67,12 +71,14 @@ class Linkedin_Bot():
             return True
 
 
-    def connect(self, name, message=None):
+    def connect(self, name):
+        """ connect to a person """
         try:
-            if message:
+            if self.message:
                 self.driver.find_element(By.XPATH,"//button[@aria-label='Add a note']").click()
                 self.im_not_robot()
-                self.driver.find_element(By.XPATH,"//textarea[@id='custom-message']").send_keys(message)
+                self.driver.find_element(By.XPATH,"//textarea[@id='custom-message']").send_keys(self.message)
+                self.im_not_robot()
             self.driver.find_element(By.XPATH,"//button[@aria-label='Send now']").click()
             self.logger.info(f" [+] Request sent to {name}!")
         except TimeoutError:
@@ -81,36 +87,62 @@ class Linkedin_Bot():
 
 
     def search(self, query):
+        """ perform search and connect() to results """
         self.logger.info(f" [+] Page {self.page_no}")
         url = f'https://www.linkedin.com/search/results/people/?keywords={query}&origin=GLOBAL_SEARCH_HEADER'
-        self.logger.info(f" [+] Searching for {query}")
         self.driver.get(url)
-        self.wait_for_element("//div[@class='entity-result']")
-        self.driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
-        self.im_not_robot(delay={'min':3, 'max':5})
-        profiles = self.driver.find_elements(By.XPATH, "//button[contains(@aria-label,'Invite')]")
-        for profile in profiles:
-            raw_name = profile.get_attribute("aria-label")
-            name = re.match(r"(?:Invite)(.*?)to",raw_name).group(1).strip()
-            print(name)
-            self.im_not_robot()
-            self.action.move_to_element(profile).click().perform()
-            self.connect(name)
-            self.im_not_robot(delay={"min":3, "max":5})
-            if self.count == self.connects_limit:
-                return None
-            else:
-                self.count +=1
-        if self.wait_for_element("//button[@aria-label='Next']"):
-            self.im_not_robot()
-            self.page_no +=1
-            self.search(url + f"&page={self.page_no}")
+        if self.cookie_expired():
+            raise LoginRequired()
+        else:
+            self.wait_for_element("//div[@class='entity-result']", timeout=5)
+            self.driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+            self.im_not_robot(delay={'min':3, 'max':5})
+            for profile in self.driver.find_elements(By.XPATH, "//button[contains(@aria-label,'Invite')]"):
+                name = re.match(r"(?:Invite)(.*?)to", profile.get_attribute("aria-label")).group(1).strip()
+                self.action.move_to_element(profile).click().perform()
+                self.connect(name)
+                self.im_not_robot(delay={"min":3, "max":5})
+                if self.count == self.connects_limit:
+                    return None
+                else:
+                    self.count +=1
+            if self.wait_for_element("//button[@aria-label='Next']"):
+                self.page_no +=1
+                self.search(url + f"&page={self.page_no}")
 
     
-    def is_captcha(self):
-        return self.wait_for_element("//h1[contains(text(),'quick security check')]")
+    def check_captcha(self):
+        if self.wait_for_element("//h1[contains(text(),'quick security check')]", timeout=5):
+            self.logger.info(" [+] Press enter after solving captcha")
+            input("Waiting:")
 
-    
+
+    def save_cookies(self):
+        cookies = self.driver.get_cookies()
+        with open("cookies.pkl", 'wb') as f:
+            pickle.dump(cookies, f)
+            self.logger.info(" [+] Cookies Saved!")
+
+
+    def cookie_expired(self):
+        if self.wait_for_element("//button[@aria-label='Sign in']", timeout=5):
+            return True
+        else:
+            return False
+
+
+    def load_cookies(self):
+        if os.path.exists("./cookies.pkl"):
+            with open("cookies.pkl", 'rb') as f:
+                cookies = pickle.load(f)
+                for cookie in cookies:
+                    self.driver.add_cookie(cookie)
+                self.logger.info(" [+] Cookies loaded!")
+                return True
+        else:
+            return False
+
+
     def login(self, username, password):   
         try:
             self.driver.get(self.login_url)
@@ -118,9 +150,7 @@ class Linkedin_Bot():
             self.driver.find_element(By.XPATH,"//input[@id='password']").send_keys(password)
             self.im_not_robot()
             self.driver.find_element(By.XPATH,"//button[@aria-label='Sign in']").click()
-            if self.is_captcha():
-                self.logger.info(" [+] Press enter after solving captcha")
-                input("Waiting:")
+            self.check_captcha()
             if self.wait_for_element("//div[contains(@class, 'feed-identity-module__actor')]", timeout=30) == None:
                 raise Exception()
         except Exception as e:
@@ -129,14 +159,19 @@ class Linkedin_Bot():
         else:
             self.logger.info(" [+] Login Successful")
             self.logged_In = True
+            self.save_cookies()
 
     
-    def take_inputs(self):
+    def take_args(self):
         parser = ArgumentParser()
         parser.add_argument('-q', '--query', dest='query', required=True, default='')
+        parser.add_argument('-c', '--connect', dest='connects_limit', default=10, type=int)
+        parser.add_argument('-m', '--message', dest='message', default='', help="e.g -m 'hello there!'")
         values = parser.parse_args()
         args_dict = vars(values)
         query = args_dict.get('query')
+        self.connects_limit = args_dict.get('connects_limit')
+        self.message = args_dict.get('message')
         if query:
             return query
         else:
@@ -146,13 +181,23 @@ class Linkedin_Bot():
     
     def main(self):
         username, password = self.load_creds()
-        query = self.take_inputs()
+        query = self.take_args()
         self.init_driver()
-        self.login(username, password)
-        if self.logged_In:
-            self.search(query)
+        if self.load_cookies():
+            try:
+                self.search(query)
+            except LoginRequired:
+                self.login(username, password)
+                if self.logged_In:
+                    self.search(query)
+        else:
+            self.login(username, password)
+            if self.logged_In:
+                self.search(query)
         self.driver.close()
 
 
 bot = Linkedin_Bot()
 bot.main()
+
+
